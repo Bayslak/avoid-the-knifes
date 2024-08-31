3ad41050997524c9bd9d6cc38bcdb86ec8e1f534
Alessandro Esposito <ale.esposito150495@gmail.com>
2024-08-31 12:58:07 +0200

    create player plugin

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
+mod player;
+
 use bevy::{input::keyboard::KeyboardInput, prelude::*};
+use player::PlayerPlugin;
 
 // Window
 const WW: f32 = 1200.0;
 const WH: f32 = 700.0;
 
 // Sprites
-const PLAYER_SPRITE_PATH: &str = "sprites/skeleton.png";
 const KNIFE_SPRITE_PATH: &str = "sprites/knife.png";
 
 const SPRITE_W: usize = 16;
 const SPRITE_H: usize = 16;
-
-// Player
-const PLAYER_SPEED: f32 = 500.0;
 
 fn main() {
     App::new()
@@ -21,7 +20,6 @@ fn main() {
                     .set(ImagePlugin::default_nearest())
                     .set(WindowPlugin {
                         primary_window: Some(Window {
-                            resizable: true,
                             focused: true,
                             resolution: (WW, WH).into(),
                             title: "Avoid the Knifes".to_string(),
@@ -30,42 +28,12 @@ fn main() {
                         ..default()
                     }),
             )
+    .add_plugins(PlayerPlugin)
     .insert_resource(Msaa::Off)
-    .add_systems(Startup, (setup_camera, spawn_player))
-    .add_systems(Update, player_movement) 
+    .add_systems(Startup, setup_camera)
     .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-}
-
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((SpriteBundle {
-        texture: asset_server.load(PLAYER_SPRITE_PATH),
-        transform: Transform {
-            scale: Vec3::splat(4.0),
-            ..default()
-        },
-        ..default()
-    }, 
-        Player { speed: PLAYER_SPEED })).insert(Name::new("Player"));
-}
-
-#[derive(Component)]
-struct Player {
-    speed: f32,
-}
-
-fn player_movement(mut player: Query<(&mut Transform, &Player)>, input: Res<ButtonInput<KeyCode>>, time: Res<Time>,) {
-
-    for (mut transform, player) in &mut player {
-        if input.pressed(KeyCode::KeyD) || input.pressed(KeyCode::ArrowRight) {
-            transform.translation.x += player.speed * time.delta_seconds();
-        }
-        
-        if input.pressed(KeyCode::KeyA) || input.pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= player.speed * time.delta_seconds();
-        }
-    }
 }
